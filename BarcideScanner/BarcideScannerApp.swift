da07dd4263b969765561a2bb6114eae129eb8dc9
jasonfangmagic <jasonfangmagic@gmail.com>
2024-06-11 14:36:33 -0400

    Initial Commit

---
+++ BarcideScanner/BarcideScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcideScannerApp.swift
+//  BarcideScanner
+//
+//  Created by Jason Fang on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcideScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarcideScanner/BarcodeScannerView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  BarcideScanner
+//
+//  Created by Jason Fang on 6/11/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 50)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not yet scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
